c4c507f97b28332eace6ab6d50d9d6b9610de173
Charmelle Gonzales <charmelle.gonzales@gmail.com>
2022-09-08 19:09:49 -0500

    Complete exercise solution

---
+++ substrings.rb
@@ -0,0 +1,17 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dictionary)
+  str_array = string.downcase.gsub(/[[:punct:]]/, "").split(" ")
+# create a new hash that totals the words that are included in the dictionary
+  dictionary.reduce(Hash.new(0)) do |result, word|
+    str_array.each do |element|
+      if element.include?(word)
+        result[word] += 1
+      end
+    end
+    result
+  end
+end
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
